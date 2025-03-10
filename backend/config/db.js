a18e3220007d4fdea6027fb0b5a26de4f941d657
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-03-10 11:08:08 +0530

    Update db.js
    Remove useUnifiedTopology: true from your MongoDB connection options.

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
         console.log("🔍 Connecting to MongoDB...");
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log("✅ MongoDB Connected Successfully!");
     } catch (err) {
