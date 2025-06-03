c00f3b003cfa5d3d60cfac31119435d9a67742ec
Sathwik Gurugubelli <gurugubellisathwik@gmail.com>
2025-06-03 21:04:22 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,10 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+
+
+
 // Hash password before saving
 UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
